2419efbaf6c3534d1d8d94c85a442b9e9a69647a
adilahamad786 <adilahamad.personally@gmail.com>
2022-11-25 12:37:31 +0530

    Create Basic server and also stablish mongoDB connection and create auth, users and posts router.

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
+const postRoute = require('./routes/posts');
+
+dotenv.config();
+
+const port = process.env.PORT;
+const mongoConnection = process.env.MONGO_URL;
+
+mongoose.connect(mongoConnection, () => {
+
+    console.log("MongoDB connected!");
+})
+
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
+app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute)
+app.use("/api/posts", postRoute)
+
+app.get('/', (req, res) => {
+    res.send('Welcome to homepage!');
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
