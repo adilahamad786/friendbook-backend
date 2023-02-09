88e33cc839bd7dfacb4c94d140a5af13d485bfc1
adilahamad786 <adilahamad.personally@gmail.com>
2023-02-09 14:12:25 +0530

    fix cross-origin problem while accessing images!

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(compression());
 
